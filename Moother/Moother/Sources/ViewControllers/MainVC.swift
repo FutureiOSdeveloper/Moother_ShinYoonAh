368cfc93b42dbe8c2c001e3a8174fc8f1e0da6b5
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-07-28 12:55:54 +0900

    add: init Setting(#1)

---
+++ Moother/Moother/Sources/ViewControllers/MainVC.swift
@@ -0,0 +1,29 @@
+//
+//  MainVC.swift
+//  Moother
+//
+//  Created by SHIN YOON AH on 2021/07/28.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
