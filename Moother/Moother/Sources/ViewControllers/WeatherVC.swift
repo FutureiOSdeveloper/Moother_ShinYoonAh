9c2541efd753c5f7576737c2557112c0c267ed3b
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-07-30 17:36:03 +0900

    chore: separator 색깔 변경(#7)

--- Moother/Moother/Sources/ViewControllers/WeatherVC.swift
+++ Moother/Moother/Sources/ViewControllers/WeatherVC.swift
@@ -54,6 +54,9 @@ class WeatherVC: UIViewController {
         tableView.register(UINib(nibName: WeatherWeekTVC.identifier, bundle: nil), forCellReuseIdentifier: WeatherWeekTVC.identifier)
         tableView.dataSource = self
         tableView.delegate = self
+        
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        tableView.separatorColor = .white.withAlphaComponent(0.3)
     }
 }
 
